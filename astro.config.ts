a25e05beb7e684e24366b943cf0b3af75fc57851
Shazril Shohaimi <shazril@bestari.io>
2025-04-28 01:25:58 +0800

    add cloudflare

--- astro.config.ts
+++ astro.config.ts
@@ -5,14 +5,18 @@ import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import { SITE } from "./src/config";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   site: SITE.website,
+
   integrations: [
     sitemap({
       filter: page => SITE.showArchives || !page.endsWith("/archives"),
     }),
   ],
+
   markdown: {
     remarkPlugins: [remarkToc, [remarkCollapse, { test: "Table of contents" }]],
     shikiConfig: {
@@ -21,19 +25,24 @@ export default defineConfig({
       wrap: true,
     },
   },
+
   vite: {
     plugins: [tailwindcss()],
     optimizeDeps: {
       exclude: ["@resvg/resvg-js"],
     },
   },
+
   image: {
     // Used for all Markdown images; not configurable per-image
     // Used for all `<Image />` and `<Picture />` components unless overridden with a prop
     experimentalLayout: "responsive",
   },
+
   experimental: {
     responsiveImages: true,
     preserveScriptOrder: true,
   },
+
+  adapter: cloudflare(),
 });
